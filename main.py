75fc29f35137565f04cca31ed2bd0f7faf7ea9be
Celia <celiamillanalemany@gmail.com>
2025-03-06 14:47:39 +0100

    feat(main.py): basic post route

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ from fastapi import FastAPI
 # en Js sería: const app = new FastAPI();
 app = FastAPI()
 
-# definimos una ruta
+# definimos una ruta para le método GET
 # estamos usando el decorador @app.get("/") para asociar la función read_root() con la ruta /.
 # los decoradores son como funciones que envuelven otras funciones
 # en Js sería: app.get("/", (req, res) => res.send({ Hello: "World" }));
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+# vamos a crear un array vacio para poder tener algún sitio donde postear algo ya que aún no tenemos la base de datos
+items = [] 
+
+# definimos una ruta con un método POST
+@app.post("/items")
+def create_items(item: str):
+    items.append(item)
+    return items
